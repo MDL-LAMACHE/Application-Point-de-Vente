1f545ee7aa96c6cedd679cbac0e3896b2dcc75ed
Luxinenglish <j.brocharddelacalle@lamache.org>
2025-01-06 23:09:36 +0100

    i forgot so...

--- js/settings.js
+++ js/settings.js
@@ -1,56 +1,17 @@
-
-const storageKey = 'theme-preference'
-
-const onClick = () => {
-    // flip current value
-    theme.value = theme.value === 'light' ? 'dark' : 'light'
-
-    setPreference()
+function setMode(oEvent){
+    document.body.classList.replace(aModes[Number(!this.checked)],aModes[Number(this.checked)]);
+    localStorage.setItem('mode',aModes[Number(this.checked)]);
+    this.parentNode.nextElementSibling.innerHTML = (this.checked)? "":""
 }
 
-const getColorPreference = () => {
-    if (localStorage.getItem(storageKey))
-        return localStorage.getItem(storageKey)
-    else
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+function loadMode(){
+    let sMode = localStorage.getItem('mode');
+    document.forms["mytheme"]["theme-mode"].checked = sMode == aModes[1];
+    setMode.call(document.forms["mytheme"]["theme-mode"])
 }
-
-const setPreference = () => {
-    localStorage.setItem(storageKey, theme.value)
-    reflectPreference()
-}
-
-const reflectPreference = () => {
-    document.firstElementChild
-        .setAttribute('data-theme', theme.value)
-
-    document
-        .querySelector('#theme-toggle')
-        ?.setAttribute('aria-label', theme.value)
-}
-
-const theme = {
-    value: getColorPreference(),
-}
-
-// set early so no page flashes / CSS is made aware
-reflectPreference()
-
-window.onload = () => {
-    // set on load so screen readers can see latest value on the button
-    reflectPreference()
-
-    // now this script can find and listen for clicks on the control
-    document
-        .querySelector('#theme-toggle')
-        .addEventListener('click', onClick)
-}
-
-// sync with system changes
-window
-    .matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', ({matches:isDark}) => {
-        theme.value = isDark ? 'dark' : 'light'
-        setPreference()
-    })
-        
+let aModes = ["mode-white", "mode-dark"];
+document.addEventListener('DOMContentLoaded',function(){
+    document.body.classList.add(aModes[0]);
+    document.forms["mytheme"]["theme-mode"].addEventListener('click', setMode);
+    loadMode()
+});
