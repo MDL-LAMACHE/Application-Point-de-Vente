273c8359cc6465dd5d7c3d50ac542bcfed9cccaf
Luxinenglish <j.brocharddelacalle@lamache.org>
2025-01-06 20:24:11 +0100

    add php bases, add all.css for fa icons and adjuste menu.css

---
+++ html/menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>C'est bancal</title>
+    <link rel="stylesheet" href="../css/menu.css">
+</head>
+<body>
+<div class="settings-menu">
+    <a class="settings-button" href="settings.php"><img src="https://cdn-icons-png.flaticon.com/256/8728/8728040.png" alt="Paramètres">Paramètres</a>
+    <div class="recipe-menu">
+        <a class="recipe-button" href="recipes.php">Recettes<img src="https://cdn-icons-png.flaticon.com/256/1828/1828859.png" alt="recette"></a>
+    </div>
+</div>
+</body>
+</html>
--- html/recipes.php
+++ html/recipes.php
@@ -10,13 +10,13 @@
 <body>
 <a href="menu.html"><i class="fa fa-arrow-left" style="color: black"></i></a>
 
-    <div class="recettes">
-<div class="recipe-options">
-  <a class="day">Jour</a>
-  <a class="week">Semaine</a>
-  <a class="month">Mois</a>
-  <a class="year">Année</a>
-</div>
+<div class="recettes">
+    <div class="recipe-options">
+        <a class="day">Jour</a>
+        <a class="week">Semaine</a>
+        <a class="month">Mois</a>
+        <a class="year">Année</a>
     </div>
+</div>
 </body>
 </html>
