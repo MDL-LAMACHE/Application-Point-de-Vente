16c93f8abe99db94979d4f86455dd5a29d7ea788
Luxinenglish <j.brocharddelacalle@lamache.org>
2025-01-07 15:34:27 +0100

    🌐 internationaliser(html) : ajouter le titre et la métadonnée de viewport dans les fichiers PHP

--- html/recipes.php
+++ html/recipes.php
@@ -2,14 +2,13 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <title>Recipes</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/recipes.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Recipes</title>
 </head>
 <body>
 <a href="menu.html"><i class="fa fa-arrow-left" style="color: black"></i></a>
-
 <div class="recettes">
     <div class="recipe-options">
         <a class="day">Jour</a>
